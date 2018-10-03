630b5536a23bd2436cf7ac1e8a2c8681ec503b8d
Leo Kacenjar <leo@openmediafoundation.org>
2018-10-03 16:29:27 -0600

    Updating plugin.

--- om-filterable-list.php
+++ om-filterable-list.php
@@ -10,13 +10,13 @@ Description: Create filterable list views with a modular Redux front end.
 require_once(plugin_dir_path(__FILE__) . "/vendor/autoload.php");
 
 use OpenMedia\FilterableList\Controller;
-use OpenMedia\FilterableList\Views\Config;
+use OpenMedia\FilterableList\ConfigUtility;
 
 add_action( 'init', function() {
-  $all_config = new Config();
-  foreach ($all_config->getViews() AS $config) {
+  $utility = new ConfigUtility();
+  foreach ($utility->getAllConfig() AS $config) {
     if (!empty($config['base_path'])) {
-      $object = get_page_by_path($config['base_path']);
+      $object = get_page_by_path(ltrim($config['base_path']));
       if (!empty($object)) {
         $regex = '^' . ltrim($config['base_path'], '/') . '/([^/]*)/?';
         $rewrite = 'index.php?page_id=' . $object->ID;
